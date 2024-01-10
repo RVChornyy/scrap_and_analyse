9e724dc5c0f906d9c16e3ec27c56fa280493b062
Rost <rv4@ukr.net>
2024-01-11 00:04:05 +0200

    create possible technologies list

---
+++ technologies.py
@@ -0,0 +1,6 @@
+POSSIBLE_TECHNOLOGIES = (
+    "PHP", "Java", "JS", "Python", "SQL", "C++", "Ruby",
+    "Docker", "ETL", "Pandas", "C#", "English", "HTML",
+    "Django", "Flask", "CSS", "FastAPI", "GIT", "1С",
+    "WEB", "API", "SEO", "CI/CD", "NodeJS", "Redis"
+)
